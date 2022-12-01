a3d00e71b22929b1b27955a4bf505fd41ab00bc8
ismartkorea <ismartkorea01@gmail.com>
2022-12-02 02:09:23 +0900

    -추가, pom.xml 수정.

---
+++ src/main/java/net/hibiznet/pms/framework/ServletInitializer.java
@@ -0,0 +1,13 @@
+package net.hibiznet.pms.framework;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EgovBootApplication.class);
+    }
+    
+}
